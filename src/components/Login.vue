66400c8ebc03f5b84d4d156672147f99589a8d88
Pinkyogre <nishanth.basil.premarajan@efrei.net>
2021-11-04 17:42:24 +0100

    ajout de login et register

---
+++ src/components/Login.vue
@@ -0,0 +1,103 @@
+<template>
+    <section class="layout">
+    <div class="container">
+      <div class="card">
+        <div class="title">
+          <h2>Connexion</h2>
+        </div>
+        <form action="">
+          <input type="text" v-model="user.email" placeholder="email" />
+          <input type="text" v-model="user.password" placeholder="password" />
+          <button type="button" @click="login(user)">Se connecter</button>
+        </form>
+        <router-link class="redirection" to="/register"
+          >Pas de compte ? S'inscrire</router-link
+        >
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+module.exports = {
+  props: {
+  },
+  data() {
+    return {
+      user: {
+        email: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    login(user) {
+      this.$emit("login", user);
+    },
+  },
+};
+</script>
+
+<style scoped>
+h2 {
+  font-family: "Indie Flower", cursive;
+  text-align: center;
+  font-size: 3rem;
+  color: darkslategrey;
+}
+
+article {
+  height: auto;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+}
+
+.card {
+  padding: 30px;
+  min-width: 50rem;
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+button {
+  max-width: 8rem;
+  border-radius: 20px;
+  padding: 5px 15px;
+  background-color: darkslategrey;
+  color: antiquewhite;
+
+  font-size: 14px;
+  
+  border: none;
+  margin: 20px;
+}
+.redirection {
+  display: block;
+  color: darkslategrey;
+  text-decoration: none;
+  margin-bottom: 5px;
+}
+
+.redirection:hover {
+  margin-bottom: 0;
+}
+
+.redirection:hover::after {
+  content: "";
+  display: block;
+  width: 187px;
+  height: 5px;
+  background: rgb(197, 118, 72);
+}
+</style>
---
+++ src/components/Register.vue
@@ -0,0 +1,118 @@
+<template>
+  <section class="layout">
+    <div class="container">
+      <div class="card">
+        <div class="title">
+          <h2>Inscription</h2>
+        </div>
+        <form action="">
+          <input type="text" v-model="newUser.email" placeholder="email" />
+          <input type="text" v-model="newUser.password" placeholder="password" />
+          <input type="text" v-model="newUser.age" placeholder="age" />
+          <select v-model="newUser.status">
+            <option value="locataire">locataire</option>
+            <option value="proprietaire">propriétaire</option>
+          </select>
+          <button @click="addUser(newUser)">S'enregistrer</button>
+        </form>
+        <router-link class="redirection" to="/login"
+          >Déjà inscrit ? Se connecter</router-link
+        >
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+module.exports = {
+  props: {},
+  data() {
+    return {
+      newUser: {
+        password: "",
+        email: "",
+        status: "",
+        age:"",
+      },
+    };
+  },
+  methods: {
+    addUser(newUser) {
+      this.$emit("add-user", newUser);
+    },
+  },
+};
+</script>
+
+<style scoped>
+h2 {
+  font-family: "Indie Flower", cursive;
+  text-align: center;
+  font-size: 3rem;
+  color: darkslategrey;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: top;
+  margin: 50px;
+}
+
+.card {
+  padding: 30px;
+  min-width: 50rem;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+input {
+  border: 2px solid white;
+  background: transparent;
+  color: white;
+  margin: 10px;
+  border-radius: 50px;
+  padding: 10px;
+  background-color: none;
+}
+
+button {
+  max-width: 8rem;
+  border-radius: 20px;
+  padding: 5px 15px;
+  background-color: darkslategrey;
+  color: antiquewhite;
+
+  font-size: 14px;
+
+  border: none;
+  margin: 20px;
+}
+
+.redirection {
+  display: block;
+  color: darkslategrey;
+  text-decoration: none;
+  margin-bottom: 5px;
+}
+
+.redirection:hover {
+  margin-bottom: 0;
+}
+
+.redirection:hover::after {
+  content: "";
+  display: block;
+  width: 191px;
+  height: 5px;
+  background: rgb(197, 118, 72);
+}
+
+
+</style>
+
+
